46b935398d413e65a6e0b0a3e7d5600cce9a042c
Mika Tammi <mika.tammi@unikie.com>
2023-06-21 19:39:00 +0300

    WIP: Add vmd-client as dependency
    * Add vmd-client as dependency of vmmanager
    * pass the path to vmd-client from command line using makeWrapper
    
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- vmmanager.nix
+++ vmmanager.nix
@@ -1,14 +1,17 @@
 {
   stdenv,
+  makeWrapper,
   qmake,
   qtbase,
   qtdeclarative,
+  vmd-client,
   wrapQtAppsHook,
 }:
 stdenv.mkDerivation rec {
   name = "vmmanager";
 
   depsBuildBuild = [
+    makeWrapper
     qmake
     qtdeclarative
     wrapQtAppsHook
@@ -17,6 +20,17 @@ stdenv.mkDerivation rec {
   depsBuildHost = [
     qtbase
     qtdeclarative
+    vmd-client
+  ];
+
+  # TODO: It is now possible to get path to vmd-client binary from
+  #       "${vmd-client}/bin/vmd-client", and it could be passed to the actual
+  #       program using command line with makeWrapper, example seen below.
+  qtWrapperArgs = [
+    "--add-flags"
+    "--vmd-client-dir"
+    "--add-flags"
+    "${vmd-client}/bin"
   ];
 
   src = ./.;
@@ -24,7 +38,8 @@ stdenv.mkDerivation rec {
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/bin; mv -t $out/bin vmmanager
+    mkdir -p $out/bin
+    mv -t $out/bin vmmanager
 
     runHook postInstall
   '';
